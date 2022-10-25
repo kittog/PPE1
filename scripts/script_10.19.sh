ad568807c7f9c36db6f5e31ef1868936caafa265
kittog <lena.gaubert@gmail.com>
2022-10-26 00:02:18 +0200

    Ajouts commentaires

--- scripts/script_10.19.sh
+++ scripts/script_10.19.sh
@@ -7,18 +7,18 @@ fi
 # trouver URL valides 
 
 FICHIER_URLS=$1
-OK=0 # URLS VALIDES
-NOK=0 # URLS NON VALIDES
-while read -r LINE ;
+OK=0 # VAR. URLS VALIDES
+NOK=0 # VAR. URLS NON VALIDES
+while read -r LINE ; # -r : recursive
 do
 echo " la ligne : $LINE "
-if [[ $LINE =∼ " ^ https ?:// " ]] # condition sur regex
+if [[ $LINE =∼ " ^ https ?:// " ]] # condition sur regex. =~ match w regex
 then
 echo " ressemble à une URL valide "
-OK=$( expr $OK + 1)
+OK=$( expr $OK + 1) # incrémentation +1
 else
 echo " ne ressemble pas à une URL valide "
-NOK=$( expr $NOK + 1)
+NOK=$( expr $NOK + 1) # incrémentation +1
 fi
 done < $FICHIER_URLS
 echo " $OK URLs et $NOK lignes douteuses "
